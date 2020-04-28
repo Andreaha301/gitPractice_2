20f1a980ca7636ed1c242ef5d2ae9def6b8cd3d3
Andreaha301 <andrea_halatrista@hotmail.com>
2020-04-28 16:12:35 -0400

    add day 1 class

---
+++ AllGood.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class AllGood {
+    public static void main(String[] args) {
+
+        //i hope everything is GOOD.
+        
+        // This line is added from Github
+    }
+
+
+}
---
+++ ItsFriday.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class ItsFriday {
+    public static void main(String[] args) {
+        System.out.println("Friday is here!!!!");
+    }
+}
